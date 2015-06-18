9cc1022795a1067a6eb7a4ba8f9688aff62a2c8f
pulsen <hunag123qwe@126.com>
2015-06-18 11:23:30 +0800

    genertor

---
+++ lib/generators/dface_xmpp/update_generator.rb
@@ -0,0 +1,5 @@
+class DfaceXmpp::UpdateGenerator < Rails::Generators::Base
+  def create_initializer_file
+    create_file "config/initializers/xmpp.rb", "# Add initialization content here"
+  end
+end
